eb600b28471d9487a147f0a32421cd98e598df19
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2026-03-05 10:18:52 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,29 @@
+import { db } from './data.js';
 
+export const API = {
+    // Pedidos
+    getPedidos: () => db.pedidos,
+    addPedido: (pedido) => db.pedidos.push(pedido),
+    
+    // Atualiza um pedido existente (MUITO IMPORTANTE PARA O FLUXO)
+    updatePedido: (id, updatedPedido) => {
+        const index = db.pedidos.findIndex(p => p.id === id);
+        if (index !== -1) {
+            db.pedidos[index] = updatedPedido;
+        }
+    },
+    
+    // Busca peça específica dentro de qualquer pedido
+    getPecaPorId: (idPeca) => {
+        for (let pedido of db.pedidos) {
+            let peca = pedido.pecas.find(p => p.id_peca === idPeca);
+            if (peca) return { peca, pedido };
+        }
+        return null;
+    },
+
+    // Usuários
+    login: ({email, senha}) => {
+        return db.usuarios.find(u => u.email == email && u.senha == senha);
+    }
+};
